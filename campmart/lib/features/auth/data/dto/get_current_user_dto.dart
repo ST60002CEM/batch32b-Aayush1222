f32aed8fe20f1c70243386a9a9c9e78af582fb89
Aayush1222 <ayushshrestha122@gmail.com>
2024-06-20 12:49:45 +0545

    adding dto

---
+++ campmart/lib/features/auth/data/dto/get_current_user_dto.dart
@@ -0,0 +1,45 @@
+import 'package:campmart/features/auth/domain/entity/auth_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'get_current_user_dto.g.dart';
+
+@JsonSerializable()
+class GetCurrentUserDto {
+  @JsonKey(name:"_id")
+  final String id;
+  final String name;
+  final String email;
+  final String password;
+
+
+
+
+  GetCurrentUserDto({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.password,
+    
+  });
+
+  AuthEntity toEntity() {
+    return AuthEntity(
+        id: id,
+        name: name,
+        email: email,
+        // lname: lname,
+        // image: image,
+        // phone: phone,
+        // batch: BatchEntity(batchId: id, batchName: ''),
+        // courses:  course.map((course) {
+        //   return CourseEntity( courseId: course, courseName: '');
+        // }).toList() ,
+        // username: username,
+        password: '');
+  }
+
+  factory GetCurrentUserDto.fromJson(Map<String, dynamic> json) =>
+      _$GetCurrentUserDtoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$GetCurrentUserDtoToJson(this);
+}
